426cef4573e3d515f111d1d3e6d7ae5bd37d8ea7
BoogieFox <mineclone14@gmail.com>
2026-04-23 14:30:09 +0200

    test erreur pmd

--- devops_tp1/src/main/java/com/example/devops_tp1/controller/HelloController.java
+++ devops_tp1/src/main/java/com/example/devops_tp1/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
+        if (true) {
+        }
+        System.out.println("hello there");
         return "hello";
     }
 }
